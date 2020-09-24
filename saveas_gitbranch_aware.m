5cac91cba3361e90311a21fec7e21865e7284a28
mattb <matthew.bennett@uclouvain.be>
2020-09-24 17:49:21 +0200

    Create matlab save(as) wrappers.
    When working with multiple branches in git, any non commited files will
    appear in your system when switching branches. Often I don't want to
    commit .mat files, and so it's annoying to have to watch that I don't
    overwrite them as I run code in different branches. One git-based
    solution is to use the worktree command, but I find that solution to be
    a bit too involved.
    
    Instead, for each branch I simply create and commit a file called
    'this_git_branch.txt' which contains the branch name. Then when using
    matlab's save(as) function, I instead use this wrapper which reads the
    branch name from the file and add it as a suffix to the saved file name.
    This means that I can keep non-commited saved files separate from one
    another without thinking. The only difference you would notice is in the
    case of saving the entire workspace - in this case I can't avoid one
    extra variable being saved along for the ride, namely the varargin that
    exists by vitue of using this function at all. Luickly the varragin in
    is merely a cell array only a few hundred bytes in size.
    
    All options passed into this wrapper function are passed to the standard
    save(as) function with only the save name altered.

---
+++ saveas_gitbranch_aware.m
@@ -0,0 +1,34 @@
+function varargout = saveas_gitbranch_aware(varargin)
+    %% documentation:
+    % When working with multiple branches in git, any non commited files will
+    % appear in your system when switching branches. Often I don't want to
+    % commit .mat files, and so it's annoying to have to watch that I don't
+    % overwrite them as I run code in different branches. One git-based
+    % solution is to use the worktree command, but I find that solution to be a
+    % bit too involved.
+    %
+    % Instead, for each branch I simply create and commit a file called
+    % 'this_git_branch.txt' which contains the branch name. Then when using
+    % matlabs saveas as function, I instead use this wrapper which reads the
+    % branch name from the file and add it as a suffix to the saved file name.
+    % This means that I can keep non-commited saved files separate from one
+    % another without thinking. The only difference you would notice is in the
+    % case of saving the entire workspace - in this case I can't avoid one
+    % extra variable being saved along for the ride, namely the varargin that
+    % exists by vitue of using this function at all. Luickly the varragin in is
+    % merely a cell array only a few hundred bytes in size.
+    %
+    % All options passed into this wrapper function are passed to the standard
+    % saveas function with only the save name altered.
+
+    %% start actual function
+    % read branch name
+    git_branch_name = textscan(fopen('this_git_branch.txt'), '%q');
+    git_branch_suffix = git_branch_name{1}{1};
+
+    % add suffix to the filename and handle for file extension
+    [filepath, filename, ext] = fileparts(varargin{2});
+    varargin{2} = [filepath, filename, '_', git_branch_suffix, ext];
+
+    % pass the all the arguemetns to the real function
+    [varargout{1:nargout}] = saveas( varargin{:} );
--- scratch1.m
+++ scratch1.m
@@ -1,20 +1,9 @@
-[4, 8, 12]/270
+randi(3)
+func_data(3)
+func_alignment_check_params
+func_data
+whos func
+whos tmp_data
+whos mean_vol
 
-screen_height_pix = 1080
-screen_height_cm = 39
-screen_distance_cm = 200
-screenVA = atand((screen_height_cm/2)/screen_distance_cm)
-pixperVA = (screen_height_pix/2)/screenVA
-r_pixperVA = pixperVA/(screen_height_pix/270)
-[.25:.25:3].*r_pixperVA
-
-figure, imagesc(circmask([270, 270], (270/2)-sigmas(end)*2))
-(((270/2)-sigmas(end)*2)/r_pixperVA)
-
-figure,
-
-sigmas
-
-tmp = fitted_models.sigma(:);
-tmp(fitted_models.r_squared<0.10)=nan;
-figure, hist(tmp,100)
+caxis auto
